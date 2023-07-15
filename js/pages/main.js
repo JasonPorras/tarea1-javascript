d82d91a67469f7b1002c0b9eb39b0ed4190c6bd8
JasonPorras <porrasartaviajason@gmail.com>
2023-07-15 00:14:48 -0600

    settings links MyAccount

--- js/pages/main.js
+++ js/pages/main.js
@@ -3,3 +3,16 @@ import { tabListId, tabsButton } from "../config/config.js";
 
 renderStructureTabs(tabListId, tabsButton);
 buttonSelected();
+
+
+const containerMain = document.querySelector(".container-main");
+
+
+const containerLinkMyAccount = document.createElement("div")
+containerLinkMyAccount.className = ("linkMyAccount")
+containerLinkMyAccount.className = ("linkMyAccount")
+containerMain.appendChild(containerLinkMyAccount);
+console.log(containerLinkMyAccount);
+console.log(containerMain);
+
+
